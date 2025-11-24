cd691d1c6cb15676608f10082a168a1a7a8a77aa
KanagawaZz <edukatsuo.ti@gmail.com>
2025-11-24 18:20:53 -0400

    Primeiro commit

---
+++ Contatos/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Contato, Grupo
+
+
+class EditarContatoForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        contato = Contato.objects.get(id= kwargs.pop('id'))
+        self.contato = contato
+        super(EditarContatoForm, self).__init__(*args, **kwargs )
+
+        self.fields['nome_contato'] = forms.CharField(max_length= 50, initial=contato.nome)
+
+        grupos = Grupo.objects.all()
+        grupos_contato = contato.grupos.all()
+
+        for grupo in grupos:
+            self.fields[f"g_{grupo.nome}"] = forms.BooleanField(required=False, initial= grupo in grupos_contato, label=f"{grupo.nome}")
+
+        telefones = contato.telefone_set.all()
+        for i, tel, in enumerate(telefones):
+            self.fields[f"tel_{i}"] = forms.CharField(max_length=14, label= f"Telefone{i+1}", initial= tel.numero)
+
+        
+        for i, email in enumerate(contato.email_set.all()):
+            self.fields[f"email_{i}"] = forms.EmailField(max_length=250, label=f"Email {i+1}", initial=contato.email_set.all() [i].endereco)
+         
+        
+
+def clean_nome_contato(self):
+    nome_contato = self.cleaned_data.get('nome_contato')
+    nomes_contatos = []
+
+    for cntt in Contato.objects.all():
+        nomes_contatos.append(cntt.nome)
+    nomes_contatos.remove(self.contato.nome)
+    if nome_contato in nomes_contatos:
+        raise forms.ValidationError("O nome escolhido ja esta sendo ultilizado")
+    return nome_contato
